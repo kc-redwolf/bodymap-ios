80a85fe2eae08df913ff1de1b28189540fc4ca70
Michael Miller <mail@mikemilla.com>
2017-03-01 22:11:52 -0500

    Better Bottom Sheet interactions

--- BodyMap/BottomSheetView.swift
+++ BodyMap/BottomSheetView.swift
@@ -8,29 +8,21 @@
 
 import UIKit
 
-class BottomSheetView: UIView {
+class BottomSheetView: UIView, UIScrollViewDelegate, UIGestureRecognizerDelegate {
     
     // MARK: Variables
     private let headerHeight:CGFloat = 68
     private let statusBarHeight:CGFloat = 20
-    private var initialTouchPoint:CGPoint! = nil
+    private var initialTouchPoint:CGFloat = 0
     private var pinnedPoint:CGFloat = 0
+    private var scrollView:UIScrollView! = nil
     
     // View State
     enum State {
         case opened
         case pinned
     }
-    var viewState:State = .pinned {
-        didSet {
-            switch viewState {
-            case .opened:
-                open(animated: true)
-            case .pinned:
-                pin(animated: true)
-            }
-        }
-    }
+    var viewState:State = .pinned
     
     // MARK: Layout Subviews
     override func layoutSubviews() {
@@ -42,15 +34,46 @@ class BottomSheetView: UIView {
     private func setup() {
         
         // Set pin point
-        pinnedPoint = superview!.bounds.height - (headerHeight + statusBarHeight)
+        pinnedPoint = superview!.bounds.height - headerHeight
         frame.origin.y = pinnedPoint
         
         // Set view pinned
         viewState = .pinned
+        
+        // Setup a pan gesture recognizer
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(viewPan))
+        addGestureRecognizer(panGesture)
+        
+        // Setup a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTap))
+        addGestureRecognizer(tapGesture)
+        
+        // Check for a scroll view subview
+        for subview in subviews {
+            
+            // Find the scroll view and break
+            if (subview.isKind(of: UIScrollView.self)) {
+                
+                // Get View
+                scrollView = subview as! UIScrollView
+                scrollView.delegate = self
+                
+                // Add pan gesture
+                let scrollPanGesture = UIPanGestureRecognizer(target: self, action: #selector(scrollViewPan))
+                scrollPanGesture.delegate = self
+                scrollView.addGestureRecognizer(scrollPanGesture)
+                
+                // End
+                continue
+            }
+        }
     }
     
     // MARK: View Settings
-    private func open(animated: Bool) {
+    func open(animated: Bool) {
+        
+        // Set state
+        viewState = .opened
         
         // Handle non-animated
         if (!animated) {
@@ -62,11 +85,10 @@ class BottomSheetView: UIView {
         UIView.animate(
             withDuration: 0.33,
             delay: 0.0,
-            usingSpringWithDamping: 0.7,
-            initialSpringVelocity: 1.0,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 1.1,
             options: .allowUserInteraction,
             animations: {
-                
                 self.frame.origin.y = self.statusBarHeight
                 
         }, completion: { (value: Bool) in
@@ -74,7 +96,10 @@ class BottomSheetView: UIView {
         })
     }
     
-    private func pin(animated: Bool) {
+    func pin(animated: Bool) {
+        
+        // Set state
+        viewState = .pinned
         
         // Handle non-animated
         if (!animated) {
@@ -86,11 +111,10 @@ class BottomSheetView: UIView {
         UIView.animate(
             withDuration: 0.33,
             delay: 0.0,
-            usingSpringWithDamping: 0.7,
-            initialSpringVelocity: 1.0,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 1.1,
             options: .allowUserInteraction,
             animations: {
-                
                 self.frame.origin.y = self.pinnedPoint
                 
         }, completion: { (value: Bool) in
@@ -99,59 +123,163 @@ class BottomSheetView: UIView {
     }
     
     // MARK: Touches
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let bottomSheetLocation = touch.location(in: self)
-            initialTouchPoint = bottomSheetLocation
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
+    @objc private func viewPan(gestureRecognize: UIPanGestureRecognizer) {
+        
+        // Get point
+        let superLocation = gestureRecognize.location(in: superview).y
+        let velocity = gestureRecognize.velocity(in: self).y
+        
+        // Get states
+        let isAboveStatusBar = frame.origin.y <= statusBarHeight
+        let isBelowPinnedPoint = frame.origin.y >= pinnedPoint
+        let threshold = superview!.bounds.height / 2
+        
+        // Handle interaction
+        switch (gestureRecognize.state) {
+        case .began:
             
-            // Get point
-            let superLocation = touch.location(in: superview)
-            let calcTouchLocation = superLocation.y - initialTouchPoint.y
+            // Get initial touch location
+            initialTouchPoint = gestureRecognize.location(in: self).y
             
-            // Get states
-//            let isAboveStatusBar = frame.origin.y <= statusBarHeight
-            let isBelowPinnedPoint = frame.origin.y >= pinnedPoint
+        case .changed:
+            
+            // Get the actual location of the user's input
+            let calculatedLocation = superLocation - initialTouchPoint
             
             // Resistence Variables
             let calcHeight = superview!.bounds.height - statusBarHeight - headerHeight
-            let downwardResistenceLocation = calcHeight * (1 + log10(calcTouchLocation / calcHeight))
-//            let upwardResistenceLocation = statusBarHeight * (1 + log10(frame.origin.y / statusBarHeight))
+            let downwardResistenceLocation = calcHeight * (1 + log10(calculatedLocation / calcHeight))
             
+            // Determine if we need to apply resistence
             if (isBelowPinnedPoint) {
                 frame.origin.y = downwardResistenceLocation
             } else {
-                frame.origin.y = calcTouchLocation
+                frame.origin.y = calculatedLocation
             }
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-        
-            // Get states
-            let isAboveStatusBar = frame.origin.y <= statusBarHeight
-            let isBelowPinnedPoint = frame.origin.y >= pinnedPoint
-            let superLocation = touch.location(in: superview)
-            let calcTouchLocation = superLocation.y - initialTouchPoint.y
-            let threshold = superview!.bounds.height / 2
+            
+        case .ended:
+            
+            // Get calulated touch location
+            let calcTouchLocation = superLocation - initialTouchPoint
             
             // Set postition
             if (isAboveStatusBar) {
-                viewState = .opened
+                open(animated: true)
             } else if (isBelowPinnedPoint) {
-                viewState = .pinned
+                pin(animated: true)
+            } else if (velocity > 800) {
+                pin(animated: true)
+            } else if (velocity < -800) {
+                open(animated: true)
             } else if (calcTouchLocation < threshold) {
-                viewState = .opened
+                open(animated: true)
             } else if (calcTouchLocation >= threshold) {
-                viewState = .pinned
+                pin(animated: true)
             }
             
+        default:
+            print("State unsupported 🤓")
         }
     }
+    
+    // MARK: Touches
+    @objc private func viewTap(gestureRecognize: UITapGestureRecognizer) {
+        switch (viewState) {
+        case .opened:
+            pin(animated: true)
+        case .pinned:
+            open(animated: true)
+        }
+    }
+    
+    var scrollTouchLocation:CGFloat! = nil
+    @objc private func scrollViewPan(gestureRecognize: UIPanGestureRecognizer) {
+        
+        let offset = scrollView.contentOffset.y
+        if (offset > 0) {
+            return
+        }
+        
+        // Get point
+        let superLocation = gestureRecognize.location(in: superview).y
+        
+        // Get states
+        let isAboveStatusBar = frame.origin.y <= statusBarHeight
+        let isBelowPinnedPoint = frame.origin.y >= pinnedPoint
+        let threshold = superview!.bounds.height / 2
+        let velocity = gestureRecognize.velocity(in: self).y
+        
+        // Handle interaction
+        switch (gestureRecognize.state) {
+        case .began:
+            
+            print("began")
+            
+            // Get initial touch location
+//            scrollTouchLocation = gestureRecognize.location(in: self).y
+            
+//            print(scrollTouchLocation)
+            
+        case .changed:
+            
+            if (scrollTouchLocation == nil) {
+                scrollTouchLocation = gestureRecognize.location(in: self).y
+            }
+            
+            // Get the actual location of the user's input
+            let calculatedLocation = superLocation - scrollTouchLocation
+            
+            frame.origin.y = calculatedLocation
+            
+            if (frame.origin.y < statusBarHeight) {
+                frame.origin.y = statusBarHeight
+                scrollView.bounces = true
+            } else {
+                scrollView.bounces = false
+            }
+            
+        case .ended:
+            
+            scrollView.bounces = true
+            
+            // Set postition
+            if (isAboveStatusBar) {
+                open(animated: true)
+            } else if (isBelowPinnedPoint) {
+                pin(animated: true)
+            } else if (velocity > 800) {
+                pin(animated: true)
+            } else if (velocity < -800) {
+                open(animated: true)
+            } else if (frame.origin.y < threshold) {
+                open(animated: true)
+            } else if (frame.origin.y >= threshold) {
+                pin(animated: true)
+            }
+            
+            scrollTouchLocation = nil
+            
+        default:
+            print("State unsupported 🤓")
+        }
+    }
+    
+    // MARK: Gesture Recognizer Delegates
+    // Needed to make scrollView gesture detectable
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+//    // MARK: ScrollView Delegate
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        
+//        let offset = scrollView.contentOffset.y
+//        
+//        if (offset <= 0) {
+//            scrollView.bounces = false
+//        } else {
+//            scrollView.bounces = true
+//        }
+//    }
 
 }
